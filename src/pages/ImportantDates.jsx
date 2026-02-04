515935574dd10ada1e3e101db01d87d77c05b452
yihsunphys <yqiu789@gmail.com>
2026-02-05 01:36:40 +0800

    update paper notification date to April 2, 2026

--- src/pages/ImportantDates.jsx
+++ src/pages/ImportantDates.jsx
@@ -4,7 +4,7 @@ const ImportantDates = () => {
             <h2 style={{ color: '#003366' }}>Important Dates</h2>
             <ul>
                 <li><strong>Abstract/Full paper submission:</strong> March 15, 2026</li>
-                <li><strong>Paper notification:</strong> April 1, 2026</li>
+                <li><strong>Paper notification:</strong> April 2, 2026</li>
                 <li><strong>Camera-ready paper due date:</strong> April 10, 2026</li>
                 <li><strong>Conference Date:</strong> April 24-25, 2026</li>
             </ul>
