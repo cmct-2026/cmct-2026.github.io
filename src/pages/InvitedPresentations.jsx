43704652710c8514495432b677734907dc78e7a0
yihsunphys <yqiu789@gmail.com>
2026-02-05 13:20:10 +0800

    update Invited Presentations heading to include "Talks &"

--- src/pages/InvitedPresentations.jsx
+++ src/pages/InvitedPresentations.jsx
@@ -1,7 +1,7 @@
 const InvitedPresentations = () => {
     return (
         <div>
-            <h2 style={{ color: '#003366' }}>Invited Presentations</h2>
+            <h2 style={{ color: '#003366' }}>Invited Talks & Presentations</h2>
             <p>Expected date of announcement: To be determined</p>
         </div>
     );
